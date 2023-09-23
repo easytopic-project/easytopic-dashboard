10c336f824267e17e3e693c82abaf92c38775274
Bruno Jucá <brunoolijuca@gmail.com>
2023-09-23 01:39:26 +0000

    Change routing settings

--- src/index.js
+++ src/index.js
@@ -13,11 +13,11 @@ app.disable('x-powered-by');
 app.use(cors());
 app.use(jsonBody());
 
-app.use('/pipeline', pipelineRouter);
+app.use('/api/pipeline', pipelineRouter);
 
-app.use('/module', moduleRouter);
+app.use('/api/module', moduleRouter);
 
-app.all('*', (req, res) => res.status(404).json('Not found'));
+app.all('/api/*', (req, res) => res.status(404).json('Not found'));
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
